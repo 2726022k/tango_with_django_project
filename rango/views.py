16c2d4279f016a90d69ee565acd668018ae63dfa
Lucas Kennington <2726022k@student.gla.ac.uk>
2024-02-09 11:22:01 +0000

    chapter 9

--- rango/views.py
+++ rango/views.py
@@ -2,25 +2,21 @@ from django.shortcuts import render
 
 # http://127.0.0.1:8000/
 
+
+from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponse
+from django.urls import reverse
+from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
 
 from rango.models import Category
 from rango.models import Page
 
-from rango.forms import CategoryForm
-from rango.forms import PageForm
-from django.shortcuts import redirect
-from django.urls import reverse
+from rango.forms import CategoryForm, PageForm, UserForm, UserProfileForm
 
-# def index(request):
-#     # Construct a dictionary to pass to the template engine as its context.
-#     # Note the key boldmessage matches to {{ boldmessage }} in the template!
-#     context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
-#     # Return a rendered response to send to the client.
-#     #  We make use of the shortcut function to make our lives easier.
-#     # Note that the first parameter is the template we wish to use.
-#     return render(request, 'rango/index.html', context=context_dict)
-#     #return HttpResponse("Rango says hey there partner! <a href='/rango/about/'>About</a>")
+
+
+
 
 def index(request):
     # Query the database for a list of ALL categories currently stored.
@@ -70,6 +66,8 @@ def show_category(request, category_name_slug):
 # Go render the response and return it to the client.
     return render(request, 'rango/category.html', context=context_dict)
 
+
+@login_required
 def add_category(request):
     form = CategoryForm()
 # A HTTP POST?
@@ -91,7 +89,7 @@ def add_category(request):
     return render(request, 'rango/add_category.html', {'form': form})
 
 
-
+@login_required
 def add_page(request, category_name_slug):
     try:
         category = Category.objects.get(slug=category_name_slug)
@@ -116,3 +114,74 @@ def add_page(request, category_name_slug):
             print(form.errors)
     context_dict = {'form': form, 'category': category}
     return render(request, 'rango/add_page.html', context=context_dict)
+
+
+
+def register(request):
+    registered = False
+    if request.method == 'POST':
+        user_form = UserForm(request.POST)
+        profile_form = UserProfileForm(request.POST)
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user = user_form.save()
+            user.set_password(user.password)
+            user.save()
+
+            profile = profile_form.save(commit=False)
+            profile.user = user
+
+            if 'picture' in request.FILES:
+                profile.picture = request.FILES['picture']
+
+            profile.save()
+            registered = True
+        else:
+            print(user_form.errors, profile_form.errors)
+    else:
+        user_form = UserForm()
+        profile_form = UserProfileForm()
+
+    return render(request, 'rango/register.html', context = {'user_form': user_form, 'profile_form': profile_form, 'registered': registered})
+
+
+
+
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username=username, password=password)
+
+        if user:
+            # Is the account active? It could have been disabled.
+            if user.is_active:
+                # If the account is valid and active, we can log the user in.
+                # We'll send the user back to the homepage.
+                login(request, user)
+                return redirect(reverse('rango:index'))
+            else:
+                # An inactive account was used - no logging in!
+                return HttpResponse("Your Rango account is disabled.")
+        else:
+            print(f"Invalid login details: {username}, {password}")
+            return HttpResponse("Invalid login details supplied.")
+    else:
+        # No context variables to pass to the template system, hence the
+        # blank dictionary object...
+        return render(request, 'rango/login.html')
+
+
+
+@login_required
+def restricted(request):
+    return render(request, 'rango/restricted.html')
+
+
+
+@login_required
+def user_logout(request):
+    # Since we know the user is logged in, we can now just log them out.
+    logout(request)
+    # Take the user back to the homepage.
+    return redirect(reverse('rango:index'))
